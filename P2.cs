16b33e8eca97270d0c1bde131483b8d8a474b73c
Anas Mazioudi <disklosr@outlook.com>
2017-11-14 07:04:06 +0100

    format code + add sln

--- P2.cs
+++ P2.cs
@@ -1,17 +1,19 @@
-public class P2 {
-    public int FindSum(){
+public class P2
+{
+    public int FindSum()
+    {
         var below = 4000000;
 
         int next = 0, current = 2, previous = 1;
         int sum = current;
 
-        while(next <= below)
+        while (next <= below)
         {
             next = current + previous;
 
-            if(next % 2 == 0)
+            if (next % 2 == 0)
                 sum += next;
-            
+
             previous = current;
             current = next;
         }
